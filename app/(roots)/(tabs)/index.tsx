5682fbb02389695448cd7dbc2db8d9195595674e
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-07 23:01:26 +0600

    Adding icons

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -6,10 +6,7 @@ const Index = () =>{
     return (
       <View style={styles.main}>
         <Text className='font-bold my-10 font-rubik text-3xl'>Welcome to RealState</Text>
-        <Link href="/SignIn">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
+        
       </View>
     )
 }
