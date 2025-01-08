3a242defe62e03a657159624c63f260371ef8424
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-09 01:16:28 +0600

    Add scrollable functionality

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -3,26 +3,34 @@ import Filters from '@/components/Filters'
 import Search from '@/components/Search'
 import icons from '@/constants/icons'
 import images from '@/constants/images'
+import { useGlobalContext } from '@/lib/global-provider'
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { FlatList } from 'react-native'
 import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Index = () => {
+  const {user} = useGlobalContext()
+
   return (
     <SafeAreaView className='bg-white h-full'>
-    <ScrollView 
-            showsVerticalScrollIndicator={false}
-            contentContainerClassName='pb-20'
-          >
-      <View className='px-5'>
+      <FlatList
+        data={[1, 2, 3, 4]}
+        renderItem={({item}) => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName='pb-32'
+        columnWrapperClassName='flex gap-5 px-5'
+        showsVerticalScrollIndicator = {false}
+        ListHeaderComponent={
+          <View className='px-5'>
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
 
-            <Image source={images.avatar} className='size-12 rounded-full' />
+            <Image source={{uri: user?.avatar}} className='size-12 rounded-full' />
             <View className='flex flex-col items-start ml-2 justify-center'>
               <Text className='text-xs font-rubik text-black-100'>Good Morning</Text>
-              <Text className='text-base font-rubik-medium text-black-300'>Ashik</Text>
+              <Text className='text-base font-rubik-medium text-black-300'>{user?.name}</Text>
             </View>
           </View>
           <Image source={icons.bell} className='size-6' />
@@ -39,11 +47,17 @@ const Index = () => {
           </View>
 
           {/* Featured Card*/}
-          <View className="flex flex-row gap-5 mt-5">
-            <FeaturedCard />
-            <FeaturedCard />
-            <FeaturedCard />
-          </View>
+          <FlatList 
+            data={[1, 2, 3, 4]}
+            renderItem={({item}) => <FeaturedCard />}
+            keyExtractor={(item) => item.toString()}
+            horizontal
+            bounces = {false}
+            showsHorizontalScrollIndicator = {false}
+            contentContainerClassName='flex gap-5 mt-5'
+
+           />
+          
         </View>
 
         {/* Our Recommendation  */}
@@ -55,14 +69,11 @@ const Index = () => {
         </View>
 
         <Filters />
-
-        {/* Card */}
-        <View className="flex flex-row gap-5 mt-2">
-          <Card />
-          <Card />
-        </View>
+        {/* Card will automatically comes */}
       </View>
-    </ScrollView>
+        }
+       />
+      
     </SafeAreaView>
   )
 }
