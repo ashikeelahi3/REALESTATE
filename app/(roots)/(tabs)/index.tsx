a0c38840a9a8e5f45e50eb8d63d257ffc2b1a64e
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-08 16:55:29 +0600

    Adding filter functionality

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -1,20 +1,70 @@
+import { Card, FeaturedCard } from '@/components/Card'
+import Filters from '@/components/Filters'
+import Search from '@/components/Search'
+import icons from '@/constants/icons'
+import images from '@/constants/images'
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { ScrollView } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const Index = () =>{
-    return (
-      <View style={styles.main}>
-        <Text className='font-bold my-10 font-rubik text-3xl'>Welcome to RealState</Text>
+const Index = () => {
+  return (
+    <SafeAreaView className='bg-white h-full'>
+    <ScrollView 
+            showsVerticalScrollIndicator={false}
+            contentContainerClassName='pb-20'
+          >
+      <View className='px-5'>
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+
+            <Image source={images.avatar} className='size-12 rounded-full' />
+            <View className='flex flex-col items-start ml-2 justify-center'>
+              <Text className='text-xs font-rubik text-black-100'>Good Morning</Text>
+              <Text className='text-base font-rubik-medium text-black-300'>Ashik</Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className='size-6' />
+        </View>
+        <Search />
+        <View className='my-5'>
+
+          {/* Featured */}
+          <View className="flex flex-row items-center justify-between">
+            <Text className='text-xl font-rubik-bold text-black-300'>Featured</Text>
+            <TouchableOpacity>
+              <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Featured Card*/}
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+
+        {/* Our Recommendation  */}
+        <View className="flex flex-row items-center justify-between">
+          <Text className='text-xl font-rubik-bold text-black-300'>Our Recommendation</Text>
+          <TouchableOpacity>
+            <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filters />
+
+        {/* Card */}
+        <View className="flex flex-row gap-5 mt-2">
+          <Card />
+          <Card />
+        </View>
       </View>
-    )
+    </ScrollView>
+    </SafeAreaView>
+  )
 }
-  
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+
 export default Index
